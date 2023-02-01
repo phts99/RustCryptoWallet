2b750789dd795609522c11eaf3659c6fcdc83061
Pedro Trindade <72526959+phts99@users.noreply.github.com>
2023-01-31 21:19:10 -0300

    Removed target  from source control

--- target/debug/deps/hello_world-4d7ecab7457d8afe.d
+++
@@ -1,5 +0,0 @@
-/home/rsgkk/Documents/Development/Rust/CrytoWallet/target/debug/deps/hello_world-4d7ecab7457d8afe.rmeta: src/main.rs
-
-/home/rsgkk/Documents/Development/Rust/CrytoWallet/target/debug/deps/hello_world-4d7ecab7457d8afe.d: src/main.rs
-
-src/main.rs:
