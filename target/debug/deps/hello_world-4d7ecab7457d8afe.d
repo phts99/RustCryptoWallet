172fe044f2c10417f1cddba87a4086a493792f22
phts99 <phts99@gmail.com>
2023-01-31 21:17:33 -0300

    Inital commit

---
+++ target/debug/deps/hello_world-4d7ecab7457d8afe.d
@@ -0,0 +1,5 @@
+/home/rsgkk/Documents/Development/Rust/CrytoWallet/target/debug/deps/hello_world-4d7ecab7457d8afe.rmeta: src/main.rs
+
+/home/rsgkk/Documents/Development/Rust/CrytoWallet/target/debug/deps/hello_world-4d7ecab7457d8afe.d: src/main.rs
+
+src/main.rs:
