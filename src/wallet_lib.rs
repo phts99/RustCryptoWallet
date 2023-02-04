2e2d9885bc491d9bd4855447ec9d396bf0c7075d
phts99 <phts99@gmail.com>
2023-02-03 23:24:03 -0300

    Add sign and send eth transaction

--- src/wallet_lib.rs
+++ src/wallet_lib.rs
@@ -3,7 +3,7 @@ use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
-use web3::{self, Web3, transports::Http};
+use web3::{self, Web3, transports::Http, types::{TransactionParameters, Res, H256, U256, H160}};
 
 pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
     let secp = secp256k1::Secp256k1::new();
@@ -15,4 +15,9 @@ pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
 pub fn establish_web3_connection(url: &str) -> Result<Web3<Http>> {
     let transport: Http = Http::new(url)?;
     Ok(web3::Web3::new(transport))
+}
+
+pub async fn sign_and_end(web3: Web3<Http>, tx_object: TransactionParameters, seckey: SecretKey) -> Result<H256> {
+    let signed = web3.accounts().sign_transaction(tx_object, &seckey).await?;
+    Ok(web3.eth().send_raw_transaction(signed.raw_transaction).await?)
 }
