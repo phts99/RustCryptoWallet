98fb35143c388e61f408f1f23e9288ccd402613f
phts99 <phts99@gmail.com>
2023-02-04 15:03:05 -0300

    Add transaction object funtion

--- src/wallet_lib.rs
+++ src/wallet_lib.rs
@@ -12,6 +12,14 @@ pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
     Ok(secp.generate_keypair(&mut rng))
 }
 
+pub fn create_transaction_object(to: H160, value: usize) -> Result<TransactionParameters> {
+    Ok(TransactionParameters{
+        to: Some(to),
+        value: U256::exp10(value), //0,1 eth
+        ..Default::default()
+    })
+}
+
 pub fn establish_web3_connection(url: &str) -> Result<Web3<Http>> {
     let transport: Http = Http::new(url)?;
     Ok(web3::Web3::new(transport))
